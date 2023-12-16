055736192a50ba4957dbc7cd3e9380960681e898
Lukas Wingerberg <bigluks@gmail.com>
2023-12-16 22:36:57 +0100

    add altlog feature

--- pureftpd/rootfs/entrypoint.sh
+++ pureftpd/rootfs/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+# WHY U NO LOG TO STDOUT?!?
+# fucking ugly workaround to get our logs acessible within k8s and a readonly root with nonroot permissions
+# Feel free to submit a PR if you have a better idea ;)
 mkfifo /tmp/pureftpd.log
 tail -f /tmp/pureftpd.log &
-
 /opt/pureftpd/sbin/pure-ftpd /opt/pureftpd/etc/pureftpd.conf
