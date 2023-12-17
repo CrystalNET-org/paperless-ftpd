38dfb445e7553b76ace6e950f5edd85bb19890f6
Lukas Wingerberg <bigluks@gmail.com>
2023-12-17 15:09:19 +0100

    switch to custom auth module

--- pureftpd/rootfs/entrypoint.sh
+++ pureftpd/rootfs/entrypoint.sh
@@ -5,4 +5,6 @@
 # Feel free to submit a PR if you have a better idea ;)
 mkfifo /tmp/pureftpd.log
 tail -f /tmp/pureftpd.log &
+
+/opt/pureftpd/sbin/pure-authd -s /tmp/ftpd.sock -r /opt/pureftpd/sbin/paperless_auth &
 /opt/pureftpd/sbin/pure-ftpd /opt/pureftpd/etc/pureftpd.conf
